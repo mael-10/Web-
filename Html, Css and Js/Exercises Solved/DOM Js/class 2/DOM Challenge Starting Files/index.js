2921fb157b33a9a6e01909f4ca18b563b73c92fb
mael-10 <maelprogrammer1@gmail.com>
2024-04-19 19:53:18 -0300

    New files about Js

---
+++ Html, Css and Js/Exercises Solved/DOM Js/class 2/DOM Challenge Starting Files/index.js
@@ -0,0 +1,11 @@
+var list = document.querySelector("ul").lastElementChild;
+// html --> body --> ul --> last 'li' in 'ul' 
+
+
+// 'querySelector' select a sepecify tags using css syntax
+list.innerHTML = "Ismael"; 
+
+// var itens = document.getElementsByTagName('li');
+// for (var i = 0; i < itens.length; i++) {
+//     alert(itens[i].textContent); // Saída: "Item 1", "Item 2", "Item 3"
+// }
