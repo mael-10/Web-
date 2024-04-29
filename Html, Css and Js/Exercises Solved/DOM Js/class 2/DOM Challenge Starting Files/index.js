0a30fd72f6a1dccfebb161fdf7f8f36f4a8f557b
mael-10 <maelprogrammer1@gmail.com>
2024-04-29 10:33:45 -0300

    Modified DOM Js class 2

--- Html, Css and Js/Exercises Solved/DOM Js/class 2/DOM Challenge Starting Files/index.js
+++ Html, Css and Js/Exercises Solved/DOM Js/class 2/DOM Challenge Starting Files/index.js
@@ -9,14 +9,16 @@ list.innerHTML = "Ismael";
 
 // Query selector select all elements tags with the same name  
 
-var colorSelector = document.getElementsByTagName("li"); //Tree variables, because are three items (It's an Array variable)
+var colorSelector = document.getElementsByTagName("li");//Tree variables, because are three items (It's an Array variable)
 
 for (var i = 0; i < colorSelector.length; i++) { //Put the array on Loop where is lower than your length
     
-    if (i === 2) { //If is equal 2 is gonna change color to red and gonna apper a pop-up 
-        colorSelector[i].style.color = "red";
-        alert(colorSelector[i].textContent); 
+    if (i === 0) { //If is equal 2 is gonna change color (a) to red and gonna apper a pop-up
+
+        document.querySelector(".list > a").style.color = "red"; //Tag a is red
+        alert(colorSelector[i].textContent);
     } else {
+
         colorSelector[i].style.color = "purple"; 
     }
 }
@@ -30,3 +32,5 @@ const box = document.getElementById("textbox");
 butto.addEventListener("click", function() { //iIf I click on button, the check-box will click too 
     box.click(); 
 })
+
+var listItem = document.querySelector("#list > .item"); // Select all elements  (It's an Array)
