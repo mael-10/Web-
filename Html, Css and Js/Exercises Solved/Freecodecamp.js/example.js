618cf788cbc3ee614794b1414cf77e3da2bdc2db
mael-10 <maelprogrammer1@gmail.com>
2024-05-20 10:38:20 -0300

    Commited change exmaple js

--- Html, Css and Js/Exercises Solved/Freecodecamp.js/example.js
+++ Html, Css and Js/Exercises Solved/Freecodecamp.js/example.js
@@ -1,3 +1,12 @@
-let num = 5;
+let character = 'Hello';
+let test = "World"; 
 
-console.log(num); 
+console.log(test); 
+
+
+let number1 = 10; 
+let number2 = 20; 
+
+console.log(number1 + number2); 
+console.log(number2 - number1); 
+console.log(number1 - number2 - number2); 
