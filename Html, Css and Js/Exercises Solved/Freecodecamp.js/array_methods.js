bf4049c8b28690f84a12069486b81dcf4e0cda7a
mael-10 <maelprogrammer1@gmail.com>
2024-05-26 14:47:30 -0300

    Add more files of Js study

--- Html, Css and Js/Exercises Solved/Freecodecamp.js/array_methods.js
+++ Html, Css and Js/Exercises Solved/Freecodecamp.js/array_methods.js
@@ -1,19 +1,19 @@
 //push
-    let test = ["azul", "verde", "rosa", "roxo"];
-    console.log(test);
+let test = ["azul", "verde", "rosa", "roxo"];
+console.log(test);
 
-    test.push("amarelo"); //Push so serve para colocar no final do array.
-    console.log(test); 
+test.push("amarelo"); //Push so serve para colocar no final do array.
+console.log(test); 
 
 
-    test[test.length - 2] = 21 + 5; // Encontrar a posição de maneira dinâmica
-    console.log(test);
+test[test.length - 2] = 21 + 5; // Encontrar a posição de maneira dinâmica
+console.log(test);
 
 //Pop 
 
-    console.log("Veja agora a diferença")
+console.log("Veja agora a diferença")
 
-    let popped = test.pop(); // O pop remove o último elemento do array
+let popped = test.pop(); // O pop remove o último elemento do array
 
-    console.log(popped); 
-    console.log(test);
+console.log(popped); 
+console.log(test);
--- Html, Css and Js/Exercises Solved/Freecodecamp.js/metho_function.js
+++ Html, Css and Js/Exercises Solved/Freecodecamp.js/metho_function.js
@@ -7,9 +7,9 @@ var boneco = {
     },
 
     falar: function(frase){
-        console.log(this.nome + frase); 
+        console.log(this.nome + frase + this.cor); 
     }
 }
 
 boneco.andar(); //boneco (object) node metho_function.js //tirar as credenciais do pc
-boneco.falar(" está falando"); 
+boneco.falar(" está falando na cor ");
