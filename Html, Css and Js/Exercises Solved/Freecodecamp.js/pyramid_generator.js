ce75ddcd23afe33281b37e7f90c1e7139930bcfe
mael-10 <maelprogrammer1@gmail.com>
2024-06-03 17:51:18 -0300

    pyramid js

--- Html, Css and Js/Exercises Solved/Freecodecamp.js/pyramid_generator.js
+++ Html, Css and Js/Exercises Solved/Freecodecamp.js/pyramid_generator.js
@@ -27,14 +27,12 @@ const counter = 8;
 const rows = []; 
 
 function padRow(rowNumber, rowCount) { //Os parâmetros servem para mudar o resultado do retorno da função de maneira dinâmica e de forma sutilmente
-    const spaceCount = rowCount -  rowNumber;
-    let space = " ";
 
-    return space.repeat(spaceCount) + character.repeat(rowNumber) + space.repeat(spaceCount);
+    return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
 for (let i = 0; i < counter; i++) {
-    rows.push(padRow(i+1, count)); //Vai repetir a quantidade de caracteres de acordo com a variáel i. Adiciona 1, pois vai começar com o valor 1 e não zero
+    rows.push(padRow(i+1, counter)); //Vai repetir a quantidade de caracteres de acordo com a variáel i. Adiciona 1, pois vai começar com o valor 1 e não zero
 }
 
 let result = ""; //Adiciona valor vazio, pois não pode concatenar variáveis com valores vazio
@@ -42,5 +40,7 @@ let result = ""; //Adiciona valor vazio, pois não pode concatenar variáveis co
 for (const linhas of rows) { //A variável linha recebe a sequência das posições do array (uma por vez) a cada vez que o loop roda
     result = result + "\n" + linhas;
 }
+
+console.log(rows);
 
 console.log(result); 
