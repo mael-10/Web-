f2728fc97cdfd49ea879a46d3b9fb5edc06361dc
mael-10 <maelprogrammer1@gmail.com>
2024-06-19 11:09:58 -0300

    Study Js

--- Html, Css and Js/Exercises Solved/Freecodecamp.js/pyramid_generator.js
+++ Html, Css and Js/Exercises Solved/Freecodecamp.js/pyramid_generator.js
@@ -1,26 +1,5 @@
-let count = 9; 
-let row = ['']; //Iniciar com um valor vazio para evitar erros 
-
-for (let i = 0; i < count; i++) {
-    row[i+1] = row[i] + "#"; //Concatena com a posição anterior, para adicionar de maneira sequêncial
-}
-
-//Tem que ser o sucessor, pois os valores no array não podem ser nulos
-
-console.log(row); 
-console.log("\n \n"); 
-
-for (const rowsElement of row) {
-    console.log(rowsElement); 
-}
-
 //node pyramid_generator.js
-
-
-
 //Pyramid generator resolved of freecodecamp!!!
-
-console.log("\n \n"); 
 
 const character = '#'; 
 const counter = 8; 
@@ -37,10 +16,9 @@ for (let i = 0; i < counter; i++) {
 
 let result = ""; //Adiciona valor vazio, pois não pode concatenar variáveis com valores vazio
 
-for (const linhas of rows) { //A variável linha recebe a sequência das posições do array (uma por vez) a cada vez que o loop roda
+for (const linhas of rows) { //A variável linha recebe a sequência das posições do array (uma por vez) a cada vez que o loop roda (variável escopo de bloco)
     result = result + "\n" + linhas;
 }
 
 console.log(rows);
-
 console.log(result); 
