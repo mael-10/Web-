05c7f553b6744c60f32e2a6bb23533b3d7ebd272
mael-10 <maelprogrammer1@gmail.com>
2024-07-01 20:50:38 -0300

    Folder deleted

---
+++ Html, Css and Js/Exercises Solved/Freecodecamp.js/Basic_knowledge/metho_function.js
@@ -0,0 +1,15 @@
+var boneco = {
+    nome: "Robozinho", 
+    cor: "Azul", 
+
+    andar: function(){
+        console.log(this.nome + " Está andando"); 
+    },
+
+    falar: function(frase){
+        console.log(this.nome + frase + this.cor); 
+    }
+}
+
+boneco.andar(); //boneco (object) node metho_function.js //tirar as credenciais do pc
+boneco.falar(" está falando na cor ");
