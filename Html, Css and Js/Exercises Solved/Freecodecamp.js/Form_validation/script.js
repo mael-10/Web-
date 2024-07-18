970ba8f0977e68b13267cb5d7becaf6112b9435b
mael-10 <maelprogrammer1@gmail.com>
2024-07-18 18:54:56 -0300

    Add form

--- Html, Css and Js/Exercises Solved/Freecodecamp.js/Form_validation/script.js
+++ Html, Css and Js/Exercises Solved/Freecodecamp.js/Form_validation/script.js
@@ -1,3 +1,5 @@
+//Values from HTML form are recivied as string
+
 const calorieCounter = document.getElementById('calorie-counter');
 const budgetNumberInput = document.getElementById('budget');
 const entryDropdown = document.getElementById('entry-dropdown');
@@ -5,3 +7,8 @@ const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
 let isError = false;
+
+
+function cleanInputString(str){
+    const regex = /\+-/; //Regex is used to form a pattern search for: Searching, Replace and Valided String
+}
