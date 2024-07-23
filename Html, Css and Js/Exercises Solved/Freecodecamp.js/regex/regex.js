fc5164b1bff8b27d0acca8db171150ede3af01df
mael-10 <maelprogrammer1@gmail.com>
2024-07-23 10:48:03 -0300

    fixed bug and css

--- Html, Css and Js/Exercises Solved/Freecodecamp.js/regex/regex.js
+++ Html, Css and Js/Exercises Solved/Freecodecamp.js/regex/regex.js
@@ -8,7 +8,7 @@ form.addEventListener('submit', function(update) { //What is 'update'? (see late
 
     console.log(nameText.match(/^teste$/)); //Return a null value if isnt corret 
 
-    if(nameText.match(/teste/) === null){
+    if(nameText.match(/^teste$/) === null){
         invalidName.style.display = 'block';
     } else {
         invalidName.style.display = 'none';
