b4426d355c7302375a204a318fa616f1a7963e37
mael-10 <maelprogrammer1@gmail.com>
2024-07-25 10:22:07 -0300

    Adicionado regex

--- Html, Css and Js/Exercises Solved/Freecodecamp.js/regex/regex.js
+++ Html, Css and Js/Exercises Solved/Freecodecamp.js/regex/regex.js
@@ -1,17 +1,22 @@
 const invalidName = document.querySelector('.invalid-camp');
 const form = document.querySelector('form');
 
-form.addEventListener('submit', function(update) { //What is 'update'? (see later)
-    update.preventDefault(); 
-    const nameText = document.getElementById('text-name').value; //Get element's value
+form.addEventListener('submit', function(event) { // Corrigido: substituído 'update' por 'event'
+    event.preventDefault(); // Corrigido: 'update' foi alterado para 'event'
+
+    const nameText = document.getElementById('text-name').value; // Obtém o valor do elemento
     console.log(nameText);
 
-    console.log(nameText.match(/^teste$/)); //Return a null value if isnt corret 
+    // Verifica se o valor corresponde exatamente à string "teste"
+    console.log(nameText.match(/^teste$/)); // Retorna null se não estiver correto
 
-    if(nameText.match(/^teste$/) === null){
+    if (nameText.match(/^teste$/) === null) {
         invalidName.style.display = 'block';
     } else {
         invalidName.style.display = 'none';
     }
-})
+});
 
+//telephone: /^\d{11}$/, 
+//username: /^[a-z\d]{11}$/i,
+//email: /^[a-z\d._%+-]+@[a-z]+\.[a-z]{2,8}\.?[a-z]{2,8}?$ 
