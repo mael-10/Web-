7aa6884e7a18c95cf3cb82e134c453fcc3bc2330
mael-10 <maelprogrammer1@gmail.com>
2024-09-05 11:26:58 -0300

    Song play

--- Html, Css and Js/Exercises Solved/Freecodecamp.js/Rock, Paper, Scissors game/script.js
+++ Html, Css and Js/Exercises Solved/Freecodecamp.js/Rock, Paper, Scissors game/script.js
@@ -1,4 +1,13 @@
 
+const rockBtn = document.getElementById("rock-btn");
+const paperBtn = document.getElementById("paper-btn");
+const scissorsBtn = document.getElementById("scissors-btn");
+const roundResultsMsg = document.getElementById('results-msg');
+const playerScoreSpanElement = document.getElementById('player-score');
+const computerScoreSpanElement = document.getElementById('computer-score');
+const winnerMsgElement = document.getElementById('winner-msg');
+const optionsContainer = document.querySelector('.options-container');
+const resetGameBtn = document.getElementById('reset-game-btn');
 let playerScore = 0;
 let computerScore = 0;
 
@@ -39,17 +48,49 @@ function getRoundResults(userOption){
 }
 
 function showResults(userOption) {
-    const roundResultsMsg = document.getElementById('results-msg');
-    const playerScoreSpanElement = document.getElementById('player-score');
-    const computerScoreSpanElement = document.getElementById('computer-score');
 
     roundResultsMsg.innerText = getRoundResults(userOption);
     playerScoreSpanElement.innerText = playerScore;
     computerScoreSpanElement.innerText = computerScore;
+
+    if(playerScore === 3){
+        winnerMsgElement.innerText = "Player has won the game!";
+        optionsContainer.style.display = 'none';
+        resetGameBtn.style.display = 'block';
+
+    } else if(computerScore === 3){
+        winnerMsgElement.innerText = "Player has won the game!";
+        optionsContainer.style.display = 'none';
+        resetGameBtn.style.display = 'block';
+
+    }
+};
+
+function resetGame(){
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreSpanElement.innerText = 0;
+    computerScoreSpanElement.innerText = 0; 
+    resetGameBtn.style.display = 'none';
+    optionsContainer.style.display = 'block';
+    winnerMsgElement.innerText = '';
+    roundResultsMsg.innerText = '';
     
-    
-
-}; 
+};
 
 
-showResults("Rock");
+rockBtn.addEventListener("click", function () {
+  showResults("Rock");
+});
+
+paperBtn.addEventListener("click", function () {
+  showResults("Paper");
+});
+
+scissorsBtn.addEventListener("click", function () {
+  showResults("Scissors");
+});
+
+resetGameBtn.addEventListener("click", function() {
+    resetGame();
+});
---
+++ Html, Css and Js/Exercises Solved/Freecodecamp.js/playing_music/script.js
@@ -0,0 +1,94 @@
+
+const playlistSongs = document.getElementById('playlist-songs');
+const playButton = document.getElementById('play');
+const pauseButton = document.getElementById('pause');
+const nextButton = document.getElementById('next');
+const previousButton = document.getElementById('previous');
+const shuffleButton = document.getElementById('shuffle');
+const allSongs = [
+  {
+    id: 0,
+    title: "Numb",
+    artist: "Link Park",
+    duration: "3:07",
+    src: "<audio controls> <source src='my-musics/Numb (Official Music Video) [4K UPGRADE] – Linkin Park.mp3' type='audio/mpeg'></audio>"
+  }, 
+  {
+    id: 1,
+    title: "Jigsaw Falling Into Place",
+    artist: "Quincy Larson",
+    duration: "4:18",
+    src: "<audio controls> <source src='my-musics/Radiohead - Jigsaw Falling Into Place.mp3' type='audio/mpeg'></audio>",
+  }, 
+  {
+    id: 2,
+    title: "Savior",
+    artist: "Rise Against",
+    duration: "4:01",
+    src: "<audio controls> <source src='my-musics/Rise Against - Savior (Official Music Video).mp3' type='audio/mpeg'></audio>",
+  },
+  {
+    id: 3,
+    title: "Cruising for a Musing",
+    artist: "Quincy Larson",
+    duration: "3:34",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/cruising-for-a-musing.mp3",
+  },
+  {
+    id: 4,
+    title: "Never Not Favored",
+    artist: "Quincy Larson",
+    duration: "3:35",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/never-not-favored.mp3",
+  },
+  {
+    id: 5,
+    title: "From the Ground Up",
+    artist: "Quincy Larson",
+    duration: "3:12",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/from-the-ground-up.mp3",
+  },
+  {
+    id: 6,
+    title: "Walking on Air",
+    artist: "Quincy Larson",
+    duration: "3:25",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/walking-on-air.mp3",
+  },
+  {
+    id: 7,
+    title: "Can't Stop Me. Can't Even Slow Me Down.",
+    artist: "Quincy Larson",
+    duration: "3:52",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/cant-stop-me-cant-even-slow-me-down.mp3",
+  },
+  {
+    id: 8,
+    title: "The Surest Way Out is Through",
+    artist: "Quincy Larson",
+    duration: "3:10",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/the-surest-way-out-is-through.mp3",
+  },
+  {
+    id: 9,
+    title: "Chasing That Feeling",
+    artist: "Quincy Larson",
+    duration: "2:43",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/chasing-that-feeling.mp3",
+  },
+];
+
+const audio = new Audio(); //Web Audio API to play the songs
+let userData = {//Storage the current song and time
+  songs: [...allSongs], //allows you to copy all elements from one array into another
+  currentSong: null,
+  songCurrentTime: 0
+};
+const printGreeting = () => { //arrow function a short way to use a function
+  console.log('Hello there!'); 
+}
+const printMessage = org => {
+  console.log(`${org} is awesome!`);
+}
+
+printMessage("freecodecamp");
