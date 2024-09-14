1c28a4b6a7c03510cbf10ca200e8cd256a474f51
mael-10 <maelprogrammer1@gmail.com>
2024-09-14 16:34:41 -0300

    New js scripts

--- Html, Css and Js/Exercises Solved/Freecodecamp.js/Exercício-1/basic_calculator/script.js
+++ Html, Css and Js/Exercises Solved/Freecodecamp.js/Exercício-1/basic_calculator/script.js
@@ -1,23 +1,29 @@
-const divisionBtn = document.getElementById('division');
-const multBtn = document.getElementById('multiplication');
-const subtractionBtn = document.getElementById('subtraction');
-const sum = document.getElementById('sum');
 const equal = document.getElementById('result');
 const cleanDisplay = document.getElementById('cleanDisplay');
 const deleteDigit = document.getElementById('deleteDigit');
 const point = document.getElementById('point');
+const allNumbers = document.querySelectorAll('.number');
+const allSignals = document.querySelectorAll('.signal');
 
-const btn0 = document.getElementById('num0');
-const btn1 = document.getElementById('num1');
-const btn2 = document.getElementById('num2');
-const btn3 = document.getElementById('num3');
-const btn4 = document.getElementById('num4');
-const btn5 = document.getElementById('num5');
-const btn6 = document.getElementById('num6');
-const btn7 = document.getElementById('num7');
-const btn8 = document.getElementById('num8');
-const btn9 = document.getElementById('num9');
-
-for(let i = 0; i <= 9; i++){
-    
+function getIdNumber(teste, e){
+    console.log(teste)
 }
+
+const basicOperations = () => {
+    for(let i = 0; i < allSignals.length; i++){
+        allSignals[i].addEventListener('click', function(){
+            getIdNumber(allSignals[i].value);
+        });
+    }
+}
+
+const iterateNumbers = () => {
+    for(let i = 0; i < allNumbers.length; i++){
+        allNumbers[i].addEventListener('click', function(){
+            getIdNumber(Number(allNumbers[i].innerText));
+        });
+    }
+}
+
+iterateNumbers();
+basicOperations();
