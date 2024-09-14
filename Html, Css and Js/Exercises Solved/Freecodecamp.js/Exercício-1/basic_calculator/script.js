004b63d53f8049afc47612a18e3fc37d939534ac
mael-10 <maelprogrammer1@gmail.com>
2024-09-14 18:09:10 -0300

    Tentando adicionar um operador

--- Html, Css and Js/Exercises Solved/Freecodecamp.js/Exercício-1/basic_calculator/script.js
+++ Html, Css and Js/Exercises Solved/Freecodecamp.js/Exercício-1/basic_calculator/script.js
@@ -4,15 +4,34 @@ const deleteDigit = document.getElementById('deleteDigit');
 const point = document.getElementById('point');
 const allNumbers = document.querySelectorAll('.number');
 const allSignals = document.querySelectorAll('.signal');
+const displayScreen = document.getElementById('display');
+let result = 0;
 
-function getIdNumber(teste, e){
-    console.log(teste)
+function getOperation(validador){
+    
+
+}
+
+function display(numberOperations){
+
+    console.log(numberOperations);
+    displayScreen.value += numberOperations;  // Atualiza o valor do display concatenando o novo valor
+    console.log(displayScreen.value);  // Exibe o valor atual do display
+
+    if(numberOperations === '+' || numberOperations === '-' || numberOperations === '*' || numberOperations === '/'){
+        result += `${numberOperations}`;
+        console.log(result)
+    } else{
+        result += numberOperations;
+    }
+ 
+    
 }
 
 const basicOperations = () => {
     for(let i = 0; i < allSignals.length; i++){
         allSignals[i].addEventListener('click', function(){
-            getIdNumber(allSignals[i].value);
+            display(allSignals[i].value);
         });
     }
 }
@@ -20,7 +39,7 @@ const basicOperations = () => {
 const iterateNumbers = () => {
     for(let i = 0; i < allNumbers.length; i++){
         allNumbers[i].addEventListener('click', function(){
-            getIdNumber(Number(allNumbers[i].innerText));
+            display(Number(allNumbers[i].innerText));
         });
     }
 }
