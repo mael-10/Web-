34c3fb2933d724ec6fb587e5d5a62abf2eebb71f
mael-10 <maelprogrammer1@gmail.com>
2024-09-14 13:45:54 -0300

    Adicionado

---
+++ Html, Css and Js/Exercises Solved/Freecodecamp.js/Exercício-1/basic_calculator/script.js
@@ -0,0 +1,23 @@
+const divisionBtn = document.getElementById('division');
+const multBtn = document.getElementById('multiplication');
+const subtractionBtn = document.getElementById('subtraction');
+const sum = document.getElementById('sum');
+const equal = document.getElementById('result');
+const cleanDisplay = document.getElementById('cleanDisplay');
+const deleteDigit = document.getElementById('deleteDigit');
+const point = document.getElementById('point');
+
+const btn0 = document.getElementById('num0');
+const btn1 = document.getElementById('num1');
+const btn2 = document.getElementById('num2');
+const btn3 = document.getElementById('num3');
+const btn4 = document.getElementById('num4');
+const btn5 = document.getElementById('num5');
+const btn6 = document.getElementById('num6');
+const btn7 = document.getElementById('num7');
+const btn8 = document.getElementById('num8');
+const btn9 = document.getElementById('num9');
+
+for(let i = 0; i <= 9; i++){
+    
+}
