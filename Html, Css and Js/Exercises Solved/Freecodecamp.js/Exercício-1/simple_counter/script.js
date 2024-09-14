43550bd2203d97dc43bc4f122ee131500913e497
mael-10 <maelprogrammer1@gmail.com>
2024-09-14 12:25:10 -0300

    Calculator html css for js

--- Html, Css and Js/Exercises Solved/Freecodecamp.js/Exercício-1/simple_counter/script.js
+++ Html, Css and Js/Exercises Solved/Freecodecamp.js/Exercício-1/simple_counter/script.js
@@ -2,7 +2,7 @@
 const buttons = document.querySelectorAll('.button > button');
 const number = document.querySelector('.number');
 
-buttons[0].addEventListener('click',  function () {
+buttons[0].addEventListener('click',  function (){
     let valueNumber = Number(number.innerText);
     console.log(valueNumber)
     valueNumber++;
@@ -11,16 +11,16 @@ buttons[0].addEventListener('click',  function () {
 
 });
 
-buttons[1].addEventListener('click',  function () {
+buttons[1].addEventListener('click',  function (){
     if(number.innerText > 0){
         let valueNumber = number.innerText;
-        console.log(valueNumber)
+        console.log(valueNumber);
         valueNumber--;
 
         number.innerHTML = valueNumber;
 
     } else {
-        alert("Não é possível número negativos")
+        alert("Não é possível número negativos");
     }
 
 });
