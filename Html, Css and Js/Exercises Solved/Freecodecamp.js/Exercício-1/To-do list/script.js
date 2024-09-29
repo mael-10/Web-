20696ab033dfd5d0ca49713159f790ec6b2c07e4
mael-10 <maelprogrammer1@gmail.com>
2024-09-29 12:16:38 -0300

    new changes on doing the to do

--- Html, Css and Js/Exercises Solved/Freecodecamp.js/Exercício-1/To-do list/script.js
+++ Html, Css and Js/Exercises Solved/Freecodecamp.js/Exercício-1/To-do list/script.js
@@ -1,7 +1,8 @@
-const dataText = document.getElementById("datetime");
-const button1 = document.querySelector('.standard-theme');
-const button2 = document.querySelector('.light-theme');
-const button3 = document.querySelector('.darker-theme');
+const dataText = document.getElementById('datetime'); 
+const buttonColor = document.querySelectorAll('.flexrow-container div');
+const body = document.querySelector('body');
+const title = document.getElementById('title');
+const datatime = document.getElementById('datetime');
 
 const zeroFill = n => {
     return ('0' + n).slice(-2); //Adiciona 0 no começo do número e corta os dois digitos finais
@@ -14,4 +15,25 @@ const interval = setInterval(() => { //Essa função recebe dois parametros
 
     dataText.innerText = dataHora;
 
-}, 1000);  
+}, 1000);
+
+function backgroundColor(bgcolor){
+    console.log(bgcolor);
+
+
+    //ternary condition
+    bgcolor === 0 ? (body.style.backgroundImage = 'linear-gradient(100deg, #575656, #062e3f)', title.style.color = '#ffffff', datatime.style.color = '#ffffff') :
+    bgcolor === 1 ? (body.style.backgroundImage = 'linear-gradient(100deg, #d4f1ff, #ffffff)', title.style.color = '#000000', datatime.style.color = '#000000') :
+    (body.style.backgroundImage = 'linear-gradient(100deg, #001214, #001f29)', title.style.color = '#ffffff', datatime.style.color = '#ffffff');
+
+    window.onload = function(){
+        
+    }
+
+}
+
+for(let i = 0; i < buttonColor.length; i++){
+    buttonColor[i].addEventListener('click', function(){
+        backgroundColor(i);
+    });
+}
