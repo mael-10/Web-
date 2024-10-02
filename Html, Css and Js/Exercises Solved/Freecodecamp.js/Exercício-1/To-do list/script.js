d7241dfd48bcdc50b50209bd1b65b5054780037c
mael-10 <maelprogrammer1@gmail.com>
2024-10-02 11:36:01 -0300

    Changes at to do list

--- Html, Css and Js/Exercises Solved/Freecodecamp.js/Exercício-1/To-do list/script.js
+++ Html, Css and Js/Exercises Solved/Freecodecamp.js/Exercício-1/To-do list/script.js
@@ -1,6 +1,10 @@
 const dataText = document.getElementById('datetime'); 
 const buttonColor = document.querySelectorAll('.flexrow-container div');
 const body = document.querySelector('body');
+const todBtn = document.querySelector('.todo-btn');
+const todoInput = document.querySelector('.todo-input');
+let taskCounter = 0;
+let allTask = [];
 
 const zeroFill = n => {
     return ('0' + n).slice(-2); //Adiciona 0 e paga somente os os dois últimos digitos
@@ -15,7 +19,6 @@ const interval = setInterval(() => { //Essa função recebe dois parametros
 }, 1000); 
 
 function backgroundColor(index){
-
     localStorage.setItem('bgIndex', index); //localStorage transoforma o valor armazenado em string
 
     console.log(index);
@@ -26,12 +29,43 @@ function backgroundColor(index){
     
 }
 
+function addTask(bgIndex){
+    const textTodo = todoInput.value;
+    taskCounter++;
+    allTask.push(
+        {
+            id: taskCounter,
+            textInput: `${textTodo}`
+        }
+    );
+    
+    //Colocar em ordem alfabética os objetos
+
+    allTask.short((a, b) => {
+        if(a.textInput < b.textInput){
+            return -1;
+        }
+
+        if(a.textInput > b.textInput){
+            return 1;
+        }
+
+        return 0;
+    });
+
+}
+
 window.onload = function(){
 
     const savedValue = localStorage.getItem('bgIndex'); //retorna uma string
     console.log(savedValue);
     backgroundColor(parseInt(savedValue)); //converte a string em número
 }
+
+todBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    addTask(localStorage.getItem('bgIndex'));
+})
 
 for(let i = 0; i < buttonColor.length; i++){
     buttonColor[i].addEventListener('click', function(){
