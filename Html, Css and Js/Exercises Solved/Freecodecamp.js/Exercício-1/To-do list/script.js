f048bee5f85130e512be59f509971b3fdbe03680
mael-10 <maelprogrammer1@gmail.com>
2024-10-20 16:13:52 -0300

    task completed

--- Html, Css and Js/Exercises Solved/Freecodecamp.js/Exercício-1/To-do list/script.js
+++ Html, Css and Js/Exercises Solved/Freecodecamp.js/Exercício-1/To-do list/script.js
@@ -4,11 +4,14 @@ const body = document.querySelector('body');
 const todoBtn = document.querySelector('.todo-btn');
 const todoInput = document.querySelector('.todo-input');
 const listTask = document.querySelector('.todo-list');
+let task = '';
 let deleteBtnTask = undefined;
 let checkBtn = undefined;
 let numberButton = 0;
 let taskCounter = 0;
 let allTask = [];
+
+let taskArray = [];
 
 const zeroFill = n => {
     return ('0' + n).slice(-2); //Adiciona 0 e paga somente os os dois últimos digitos
@@ -73,6 +76,11 @@ function verifyInput(verifyTextTodo){
     }
 }
 
+function isDuplicateTask(newTaskText) {
+    // Verifica se já existe uma tarefa com o mesmo texto (ignorando maiúsculas e minúsculas)
+    return allTask.some(task => task.textInput.toLowerCase() === newTaskText.toLowerCase());
+}
+
 function deleteTask(){
     console.log('oi');
 }
@@ -107,38 +115,67 @@ function validateBtn() {
             checkBtn.hasListener = true;
         }
     });
-
-    
 }
 
+function savedValidate(oldTaskList) {
+    if (oldTaskList.length !== 0) { // verifica se existe valores no array
+        const newTaskList = document.querySelectorAll('.todo'); 
+
+        newTaskList.forEach((newTask, indexE) => {
+            const newCharTask = newTask.querySelector('.todo-item');
+
+            oldTaskList.forEach((oldTask, indexI) => {
+                const oldCharTask = oldTask.querySelector('.todo-item');
+
+                if (newCharTask.innerText === oldCharTask.innerText) { 
+                    // Substituir o nó inteiro da nova lista pelo da lista antiga
+                    newTask.replaceWith(oldTaskList[indexI]);
+                }
+            });
+        });
+    }
+}
 
 function addTask(){
 
     listTask.innerHTML = '';
+    taskArray = []
 
     for(i = 0; i < allTask.length; i++){
-        listTask.insertAdjacentHTML('beforeend', 
-            `<div id='btn-${taskCounter}' class='todo standard-todo'>  
-                <li class='todo-item'>${allTask[i].textInput}</li>
-                <button class='check-btn darker-button' type='button'>
-                    <i class='fas fa-check'></i>
-                </button>
-                <button class='delete-btn darker-button' type='button'>
-                    <i class='fas fa-trash'></i>
-                </button>
-            </div>`
-        );
+         
+        //Push transforma um string em um array
+        taskArray.push(` 
+            <div id='btn-${i+1}' class='todo standard-todo'>  
+            <li class='todo-item'>${allTask[i].textInput}</li>
+            <button class='check-btn darker-button' type='button'>
+                <i class='fas fa-check'></i>
+            </button>
+            <button class='delete-btn darker-button' type='button'>
+                <i class='fas fa-trash'></i>
+            </button>
+        </div>`)
     }
+
+    listTask.innerHTML = taskArray.join(""); //Junta o array em uma string
+
+    //console.log(task) // Array Antigo
 }
 
 function preperTask(bgIndex){
 
-    const textTodo = todoInput.value.trim();
+    const task = document.querySelectorAll('.todo');
+    const textTodo = todoInput.value;
 
     if(verifyInput(textTodo)){
         todoInput.value = '';
-        return 
-    } 
+        return; 
+    }
+
+    if (isDuplicateTask(textTodo)) {
+        alert('Task already exists! Please enter a new task.');
+        todoInput.value = '';
+        return;
+    }
 
     taskCounter++;
     allTask.push(
@@ -153,9 +190,9 @@ function preperTask(bgIndex){
     UpperCase(textTodo); //Colocar o primeiro nome em letra maiúscula
     alphabeticalOrder(allTask); //Colocar em ordem alfabética os objeto
     addTask(); //adiciona a tarefa
+    savedValidate(task); //salva o valor do check
     backgroundColor(bgIndex); // Muda todo o background colocar das cores
     validateBtn(); //Valida o botão the check
-
 }
 
 window.onload = function(){
@@ -165,6 +202,7 @@ window.onload = function(){
 }
 
 todoBtn.addEventListener('click', function(e){
+    
   e.preventDefault();
     preperTask(parseInt(localStorage.getItem('bgIndex')));
 })
