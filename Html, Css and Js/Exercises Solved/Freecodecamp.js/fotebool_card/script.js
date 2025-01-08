bc1adad51c2d7ede84588872d4315a1972d7e724
mael-10 <maelprogrammer1@gmail.com>
2025-01-08 10:15:28 -0300

    cards

---
+++ Html, Css and Js/Exercises Solved/Freecodecamp.js/fotebool_card/script.js
@@ -0,0 +1,17 @@
+const teamName = document.getElementById("team");
+const typeOfSport = document.getElementById("sport");
+const worldCupYear = document.getElementById("year");
+const headCoach = document.getElementById("head-coach");
+const playerCards = document.getElementById("player-cards");
+const playersDropdownList = document.getElementById("players");
+const myFavoriteFootballTeam = {
+    team: "Argentina",
+    sport: "Football",
+    year: 1986,
+    isWorldCupWinner: true,
+    headCoach: {
+        coachName: "Carlos Bilardo",
+        matches: 7,
+    },
+    players: []
+};
